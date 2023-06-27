ba0fec037a05d7a68751e990a0953c7156a58b1c
Marcos Tamburi <marcose027@hotmail.com>
2023-06-27 18:15:17 -0300

    commit nro 2(final) Lc1 tp integrado en grupo

--- LabComputacion/TpIntegrador.c
+++ LabComputacion/TpIntegrador.c
@@ -1,8 +1,11 @@
+//Miembros del grupo: Marcos Tamburi - Nicolas Robles - Mariano Gerosa - Franco Balduini
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 #define maxClientes 10
+#define maxOperaciones 10
 
 int numPassCuenta[maxClientes][2];
 char nombres[maxClientes][50];
@@ -10,12 +13,196 @@ float saldos[maxClientes];
 bool estado[maxClientes];
 int i, opc;
 
+int opRealizadas = 0;
+
 void depositoCuenta();
 void extraccionCuenta();
 void consultaSaldoCuenta();
 void operacionesRalizadasSaldo();
+char lineaDePuntos();
+void definirArrays();
 
 int main()
+{
+    definirArrays();
+
+    do
+    {
+        int numCuentaIngresada, intentos = 0, passCuentaIngresada, encontrado = 0, user;
+        opRealizadas = 0;
+        do
+        {
+            lineaDePuntos();
+            printf("Hola, bienvenido al sistema de cajero automatico \n");
+            printf("Ingrese su n%cmero de cuenta: \n", 163);
+            lineaDePuntos();
+            scanf("%d", &numCuentaIngresada);
+            lineaDePuntos();
+
+            lineaDePuntos();
+            printf("Ingrese la contrase%ca \n", 164);
+            lineaDePuntos();
+            scanf("%d", &passCuentaIngresada);
+            lineaDePuntos();
+
+            for (i = 0; i < maxClientes; i++)
+            {
+                if (numCuentaIngresada == numPassCuenta[i][1] && passCuentaIngresada == numPassCuenta[i][2])
+                {
+                    encontrado = 1;
+                    user = i;
+                }
+            }
+
+            intentos++;
+            if (intentos == 3)
+            {
+                for (i = 0; i < maxClientes; i++)
+                {
+                    if (numCuentaIngresada == numPassCuenta[i][1])
+                    {
+                        estado[i] = false;
+                    }
+                }
+            }
+
+        } while (encontrado == 0 && intentos < 3);
+
+        if (estado[user] == false)
+        {
+            lineaDePuntos();
+            printf("Su cuenta ha sido bloqueada\nComun%cquese con la entidad bancaria para su restablecimiento\n", 161);
+            lineaDePuntos();
+        }
+
+        while (encontrado == 1 && estado[user] == true)
+        {
+            do
+            {
+
+                if (opRealizadas <= maxOperaciones)
+                {
+
+                    lineaDePuntos();
+                    printf("1-Dep%csito \n", 162);
+                    printf("2-Extracci%cn \n", 162);
+                    printf("3-Consultar saldo \n");
+                    printf("4-Mostrar la cantidad de operaciones realizadas y el saldo actual \n");
+                    printf("5-Salir\n");
+                    lineaDePuntos();
+                    scanf("%d", &opc);
+                    lineaDePuntos();
+
+                    switch (opc)
+                    {
+                    case 1:
+                        depositoCuenta(saldos, user);
+                        break;
+                    case 2:
+                        extraccionCuenta(saldos, user);
+                        break;
+                    case 3:
+                        consultaSaldoCuenta(saldos, user);
+                        break;
+                    case 4:
+                        operacionesRalizadasSaldo(saldos, user);
+                        break;
+                    case 5:
+                        lineaDePuntos();
+                        printf("\n\t\tHasta luego\n");
+                        encontrado = 0;
+                        break;
+                    default:
+                        printf("Opci%cn no v%clida \n", 162, 160);
+                        break;
+                    }
+                }
+                else
+                {
+                    lineaDePuntos();
+                    printf("\nLleg%c al l%cmite de operaciones. Fin. Gracias!\n", 162, 161);
+                    lineaDePuntos();
+                    encontrado = 0;
+                }
+
+            } while (opc != 5 && opRealizadas < maxOperaciones);
+        }
+
+        do
+        {
+            printf("\n\n");
+            lineaDePuntos();
+            printf("Desea seguir realizando operaciones? 1-Si| 0-No \n");
+            scanf("%d", &opc);
+            lineaDePuntos();
+        } while (opc <= 0 && opc >= 1);
+
+    } while (opc != 0);
+
+    system("pause");
+    return 0;
+}
+
+void depositoCuenta(float arraySaldos[], int indice)
+{
+    float deposito = 0;
+    lineaDePuntos();
+    printf("Ingrese el dinero a depositar \n");
+    lineaDePuntos();
+    scanf("%f", &deposito);
+    lineaDePuntos();
+    arraySaldos[indice] += deposito;
+    opRealizadas++;
+}
+
+void extraccionCuenta(float arraySaldos[], int indice)
+{
+    float extraccion = 0;
+    lineaDePuntos();
+    printf("Ingrese cuanto dinero desea retirar.\n");
+    lineaDePuntos();
+    scanf("%f", &extraccion);
+    lineaDePuntos();
+
+    if (extraccion < arraySaldos[indice])
+    {
+        arraySaldos[indice] -= extraccion;
+        opRealizadas++;
+    }
+    else
+    {
+        printf("No se puede extraer mas plata de lo que tiene la cuenta\n");
+    }
+}
+
+void consultaSaldoCuenta(float arraySaldos[], int indice)
+{
+    lineaDePuntos();
+    printf("Aqui tienes el saldo disponible en la cuenta \n");
+    lineaDePuntos();
+    printf("%.2f\n", arraySaldos[indice]);
+    lineaDePuntos();
+    opRealizadas++;
+}
+
+void operacionesRalizadasSaldo(float arraySaldos[], int indice)
+{
+    lineaDePuntos();
+    printf("La cantidad de operaciones realizadas son: %d\n", opRealizadas);
+    printf("El saldo actual es de: $%.2f\n", arraySaldos[indice]);
+    lineaDePuntos();
+}
+
+char lineaDePuntos()
+{
+    for (i = 0; i < 21; i++)
+    {
+        printf(" -");
+    }
+    printf("\n");
+}
+
+void definirArrays()
 {
     // Nro de cuenta
     numPassCuenta[0][1] = 10;
@@ -53,77 +240,15 @@ int main()
     estado[8] = true;
     estado[9] = true;
 
-
-    int numCuentaIngresada, intentos = 0, passCuentaIngresada, encontrado = 0, user;
-
-    printf("Hola, bienvenido al sistema de cajero automatico \n");
-
-    do
-    {
-        printf("Ingrese su n%cmero de cuenta: \n", 163);
-        scanf("%d", &numCuentaIngresada);
-
-        printf("Ingrese la contrase%ca \n", 164);
-        scanf("%d", &passCuentaIngresada);
-
-        for (i = 0; i < maxClientes; i++)
-        {
-            if (numCuentaIngresada == numPassCuenta[i][1] && passCuentaIngresada == numPassCuenta[i][2])
-            {
-                encontrado = 1;
-                user = i;
-            }
-        }
-        intentos++;
-    } while (encontrado == 0 && intentos < 3);
-
-    while (encontrado == 1 && estado[user] == true)
-    {
-        do
-        {
-            printf("1-Dep%csito \n", 162);
-            printf("2-Extracci%cn \n", 162);
-            printf("3-Consultar saldo \n");
-            printf("4-Mostrar la cantidad de operaciones realizadas y el saldo actual \n");
-            printf("5-Salir\n");
-            scanf("%d", &opc);
-
-            switch (opc)
-            {
-            case 1:
-                depositoCuenta();
-                break;
-            case 2:
-                extraccionCuenta();
-                break;
-            case 3:
-                consultaSaldoCuenta();
-                break;
-            case 4:
-                operacionesRalizadasSaldo();
-                break;
-            default:
-                printf("Opcion no valida \n");
-                break;
-            }
-        } while (opc != 5);
-    }
-
-    return 0;
-}
-
-void depositoCuenta()
-{
-}
-
-void extraccionCuenta()
-{
-}
-
-void consultaSaldoCuenta()
-{
-}
-
-void operacionesRalizadasSaldo()
-{
+    // Saldo de los clientes
+    saldos[0] = 0;
+    saldos[1] = 0;
+    saldos[2] = 0;
+    saldos[3] = 0;
+    saldos[4] = 0;
+    saldos[5] = 0;
+    saldos[6] = 0;
+    saldos[7] = 0;
+    saldos[8] = 0;
+    saldos[9] = 0;
 }
